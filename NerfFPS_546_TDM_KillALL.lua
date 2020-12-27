72f5c72b17a572433cc2ce930f4b6841e985182c
Tek9x <sean.schramm@live.com>
2020-12-27 01:59:46 -0500

    Update NerfFPS_546_TDM_KillALL.lua

--- NerfFPS_546_TDM_KillALL.lua
+++ NerfFPS_546_TDM_KillALL.lua
@@ -17,7 +17,7 @@ for _, v in pairs(loopList) do
     print("[Current Target]: " .. v.name)
     repeat
         wait(1)
-        thisPlayer.Character.HumanoidRootPart.CFrame = v.Character.HumanoidRootPart.CFrame * CFrame.new(0,0,8)
+        --thisPlayer.Character.HumanoidRootPart.CFrame = v.Character.HumanoidRootPart.CFrame * CFrame.new(0,0,8)
         event:FireServer(149306230, v.Character, 100, "VULCAN", false)
         --count = count + 1
         print("[Targets Heath] " .. v.Character.Humanoid.Health)
