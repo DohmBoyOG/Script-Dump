7117fcfe87c4d7605431230c1eff8bffb6bea285
Tek9x <sean.schramm@live.com>
2020-12-28 01:32:56 -0500

    Update NerfFPS_546_GunMod.lua

--- NerfFPS_546_GunMod.lua
+++ NerfFPS_546_GunMod.lua
@@ -24,15 +24,15 @@ for _, v in pairs(weapList) do
         mods = require(v).stats
 
         mods.boltaction = false
-        mods.damage = 0 -- [Default is 100]
-        mods.maxclip = 0
-        mods.stored = 0
+        mods.damage = 9999 -- [Default is 100]
+        mods.maxclip = 9999
+        mods.stored = 9999
         mods.bullet = "sword" -- [dart], [ball], [stefan], [elite], [mega], [rocket], [sword] <-- lol? --
-        mods.range = 0
-        mods.rate = 0
+        mods.range = 3000
+        mods.rate = 0.15
         mods.zoom = 70 -- [Be careful messing with this one. you could end up too zoomed!] --
-        mods.weight = 0
-        mods.firemode = 0 -- [0] Single Shot , [1] Semi Auto , [2] Full Auto --
+        mods.weight = 0 -- [ Don't set weight to zero, higher number is lesser weight!!] --
+        mods.firemode = 2 -- [0] Single Shot , [1] Semi Auto , [2] Full Auto --
         mods.spread = Vector3.new(0, 0, 0) -- [all zeros no spread?] --
         mods.aimspread = Vector3.new(0, 0, 0) -- [all zeros no spread?] --
         mods.firepitch = 0 -- [Have no idea what this does] --
