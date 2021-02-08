97021fc2b748755b91c467d9a09766c94b70e70a
Tek9x <sean.schramm@live.com>
2021-02-07 23:30:25 -0500

    Update RumbleQuest_AutoDungeon_DohmScripts_AutoExec_Settings_Public.lua

--- RumbleQuest_AutoDungeon_DohmScripts_AutoExec_Settings_Public.lua
+++ RumbleQuest_AutoDungeon_DohmScripts_AutoExec_Settings_Public.lua
@@ -1,7 +1,9 @@
-getgenv().Auto = true
-getgenv().Difficulty = 'Easy'
-getgenv().PartyOnly = true
-getgenv().Hardcore = false
-getgenv().Location = 'Caves'
+--[ Keep in mind, the options still need to be unlocked in game, for you to create a Dungeon of that type and Difficulty Mode]
+
+getgenv().Auto = true --[ No need to change this setting.]
+getgenv().Difficulty = 'Easy' --[ Easy, Medium, Hard, Expert ]
+getgenv().PartyOnly = true --[True/False]
+getgenv().Hardcore = false --[True/False]
+getgenv().Location = 'Caves' --[ Dungeon Name]
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/DohmBoyOG/Script-Dump/main/RumbleQuest_AutoDungeon_DohmScripts_AutoExec_Public.lua"))()
