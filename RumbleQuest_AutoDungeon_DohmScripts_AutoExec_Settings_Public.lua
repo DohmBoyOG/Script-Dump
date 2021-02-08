9816bf510abf9b35a33bf4827594de1344b7ff1b
Tek9x <sean.schramm@live.com>
2021-02-07 23:31:43 -0500

    Update RumbleQuest_AutoDungeon_DohmScripts_AutoExec_Settings_Public.lua

--- RumbleQuest_AutoDungeon_DohmScripts_AutoExec_Settings_Public.lua
+++ RumbleQuest_AutoDungeon_DohmScripts_AutoExec_Settings_Public.lua
@@ -1,4 +1,5 @@
 --[ Keep in mind, the options still need to be unlocked in game, for you to create a Dungeon of that type and Difficulty Mode]
+--[ Dungeon Creation Starts at 10 seconds, Auto Attack at 5 seconds]
 
 getgenv().Auto = true --[ No need to change this setting.]
 getgenv().Difficulty = 'Easy' --[ Easy, Medium, Hard, Expert ]
