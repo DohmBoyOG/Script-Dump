68345934bd31c85b31595fcb0abef2cafb3a184f
Tek9x <sean.schramm@live.com>
2021-02-07 23:25:59 -0500

    Create RumbleQuest_AutoDungeon_DohmScripts_AutoExec_Settings_Public.lua

---
+++ RumbleQuest_AutoDungeon_DohmScripts_AutoExec_Settings_Public.lua
@@ -0,0 +1,7 @@
+getgenv().Auto = true
+getgenv().Difficulty = 'Easy'
+getgenv().PartyOnly = true
+getgenv().Hardcore = false
+getgenv().Location = 'Caves'
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DohmBoyOG/Script-Dump/main/RumbleQuest_AutoDungeon_DohmScripts_AutoExec_Public.lua"))()
