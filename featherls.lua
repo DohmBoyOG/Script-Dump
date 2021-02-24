9edb73683b93e7003f9706ddc78f40b4540df478
Tek9x <sean.schramm@live.com>
2021-02-24 09:30:15 -0500

    Create featherls.lua

---
+++ featherls.lua
@@ -0,0 +1,3 @@
+getgenv().Text = 'DohmScripts are Awesome!'
+
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/DohmBoyOG/Script-Dump/main/FeatherFamily_Troll_DohmScripts_Public.lua'),true))()
