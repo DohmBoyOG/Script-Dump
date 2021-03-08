ca89cddd587788323f1c377c78dee47de52f87a2
Tek9x <sean.schramm@live.com>
2021-03-08 00:39:38 -0500

    Create knifeLS.lua

---
+++ knifeLS.lua
@@ -0,0 +1,7 @@
+getgenv().Auto = false --[ True or False]
+getgenv().BoxType = "Knife" --[ Knife, Pistol, Animal]
+getgenv().Option = "One" --[ One, Two, Three, Four]
+getgenv().Speed = 0.6 --[ How Fast it Goes]
+
+
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/DohmBoyOG/Script-Dump/main/MurderMysteryX_AutoBox_DohmScripts_Public.lua'),true))()
