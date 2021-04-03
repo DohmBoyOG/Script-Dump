94dfa4a0cca72235cba2d89fea983e8f85df3c5f
Tek9x <sean.schramm@live.com>
2021-04-03 01:11:05 -0400

    Create FreeADMIN_DohmScripts_Public.lua

---
+++ FreeADMIN_DohmScripts_Public.lua
@@ -0,0 +1,13 @@
+local vip = game:GetService("Workspace")["VIP Rooms"]:GetDescendants()
+local trash = {"VIPDoor", "VIP", "Union", "Mega VIP", "Epic VIP", "Floor", "Rich VIP", "Ultra VIP"}
+
+for i = 1, #trash do
+    v = trash[i]
+    for o, p in pairs(vip) do
+        if p.Name == v then
+            p:Destroy()
+        end
+    end
+end
+
+
