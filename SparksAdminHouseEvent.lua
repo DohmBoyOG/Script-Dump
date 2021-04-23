bf3f8a7d5f9a559f9f833f84a0c43a3e53eaffb6
Tek9x <sean.schramm@live.com>
2021-04-22 20:25:37 -0400

    Create SparksAdminHouseEvent.lua

---
+++ SparksAdminHouseEvent.lua
@@ -0,0 +1,10 @@
+local thisPlayer = game:GetService("Players").LocalPlayer
+
+for _, v in pairs(game:GetService("Workspace"):GetDescendants()) do
+    if v:IsA("TouchTransmitter") and string.match(v.Parent.Name, "SparksChest") then
+        firetouchinterest(thisPlayer.Character.HumanoidRootPart, v.Parent, 0)
+        wait()
+        firetouchinterest(thisPlayer.Character.HumanoidRootPart, v.Parent, 1)
+    end
+end
+
